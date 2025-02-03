0efa8be2ca673fc5a80538ec68d9a9e55cd3d9c6
Daniel Monteiro <158519099+danielMonteiro84@users.noreply.github.com>
2025-02-03 19:54:26 +0000

    criando card list e estilizando list

--- taskflow/src/pages/list/index.tsx
+++ taskflow/src/pages/list/index.tsx
@@ -1,11 +1,81 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import { style } from "./styles";
+import { Input } from "../../components/Input";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialIcons } from "@expo/vector-icons";
+
+type PropCard = {
+  item: number;
+  title: string;
+  description: string;
+  flag: "pendente" | "feito";
+};
+
+const data: Array<PropCard> = [
+  {
+    item: 0,
+    title: "Realizar a lição de casa",
+    description: "pagina 10 a 20",
+    flag: "pendente",
+  },
+
+  {
+    item: 1,
+    title: "Passear com o cachorro",
+    description: "Luck tem que passear as 17h",
+    flag: "pendente",
+  },
+
+  {
+    item: 2,
+    title: "Roupas na custureira",
+    description: "Roupas ficaram prontas dia 20",
+    flag: "pendente",
+  },
+];
 
 export default function List() {
+  const _listCard = (item: PropCard) => {
+    return (
+      <TouchableOpacity style={style.card}>
+        <View style={style.listCard}>
+          <View>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={style.tela}>
-      <Text>Lista </Text>
+      <LinearGradient
+        colors={["#F4C27F", "#D8605B"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        //style={{ flex: 1 }}
+      >
+        <View style={style.header}>
+          <Text style={style.saudacao}>
+            Bom dia, <Text style={{ fontWeight: "bold" }}>Daniel </Text>{" "}
+          </Text>
+          <View style={style.boxInput}>
+            <Input IconLeft={MaterialIcons} IconLeftName="search" />
+          </View>
+        </View>
+      </LinearGradient>
+      <View style={style.boxList}>
+        <FlatList
+          data={data}
+          style={{ marginTop: 40, paddingHorizontal: 30 }}
+          keyExtractor={(item, index) => item.item.toString()}
+          renderItem={({ item, index }) => {
+            return _listCard(item);
+          }}
+        />
+      </View>
     </View>
   );
 }
--- taskflow/src/pages/list/styles.ts
+++ taskflow/src/pages/list/styles.ts
@@ -1,11 +1,54 @@
+import { ScaleFromCenterAndroid } from "@react-navigation/stack/lib/typescript/commonjs/src/TransitionConfigs/TransitionPresets";
 import { StyleSheet, Dimensions } from "react-native";
 
 export const style = StyleSheet.create({
   tela: {
-    backgroundColor: "green",
-    width: "100%",
-    height: "100%",
+    flex: 1,
+    backgroundColor: "#F4C27F99",
     alignItems: "center",
+  },
+
+  header: {
+    width: "100%",
+    height: Dimensions.get("window").height / 6,
+    padding: 30,
     justifyContent: "center",
+  },
+  saudacao: {
+    fontSize: 18,
+    marginTop: 20,
+    marginLeft: 15,
+    fontFamily: "PoppinsRegular",
+    fontWeight: "semibold",
+    color: "#fff",
+  },
+  boxInput: {
+    width: "100%",
+  },
+  boxList: {
+    flex: 1,
+    width: "100%",
+  },
+  card: {
+    width: "100%",
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    justifyContent: "center",
+    padding: 15,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 5,
+    marginBottom: 15,
+  },
+
+  listCard: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
